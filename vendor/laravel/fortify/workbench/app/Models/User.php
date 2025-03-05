e5ca622c6f75a17147176f60e81fbf87d279e03e
pasahgroup <info@pasah.net>
2025-03-05 14:09:57 +0300

    05 march 2025

--- vendor/laravel/fortify/workbench/app/Models/User.php
+++ vendor/laravel/fortify/workbench/app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class user extends Authenticatable
+class User extends Authenticatable
 {
     /**
      * The attributes that are mass assignable.
