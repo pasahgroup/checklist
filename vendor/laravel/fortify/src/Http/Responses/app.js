f3d6100a12fdeb0c66c39e180275273be23a10ad
pasahgroup <info@pasah.net>
2025-03-05 13:53:55 +0300

    05 march 2025

---
+++ vendor/laravel/fortify/src/Http/Responses/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import Chat from './components/Chat.vue';
+import App from './App.vue'
+const app = createApp(App);
+app.component('Chat', Chat);
+
+app.mount("#app");
